0c8f6fd09c45a552fe419662c6a2c7307e08b4f3
Martimbul Samkrinius Hutagalung <samkriniushutagalung@gmail.com>
2023-05-31 14:49:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/votexy/home/InfoCalonActivity.kt
@@ -0,0 +1,12 @@
+package com.example.votexy.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.votexy.R
+
+class InfoCalonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_calon)
+    }
+}
---
+++ app/src/main/java/com/example/votexy/startApp/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.votexy.startApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.votexy.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
