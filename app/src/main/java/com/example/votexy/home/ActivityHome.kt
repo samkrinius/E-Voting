2ad77e1842cb1ef427952b2690c8429007a82477
Martimbul Samkrinius Hutagalung <samkriniushutagalung@gmail.com>
2023-06-06 17:00:35 +0700

    Penambahan layout dari aplikasi

---
+++ app/src/main/java/com/example/votexy/home/ActivityHome.kt
@@ -0,0 +1,13 @@
+package com.example.votexy.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.votexy.R
+
+class ActivityHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/example/votexy/startApp/SplahActivity.kt
+++ app/src/main/java/com/example/votexy/startApp/SplahActivity.kt
@@ -1,12 +1,21 @@
 package com.example.votexy.startApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.votexy.R
 
 class SplahActivity : AppCompatActivity() {
+
+    private val  SPLASH_TIME_OUT:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splah)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
